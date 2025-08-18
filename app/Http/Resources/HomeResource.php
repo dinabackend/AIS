c77247b52d80835747a20697d915781776fd47c9
Bexruz999 <bexruzfatullayev999@gmail.com>
2025-08-18 17:28:58 +0500

    feat: refactor HomeResource to correctly assign company content names and texts for localization

--- app/Http/Resources/HomeResource.php
+++ app/Http/Resources/HomeResource.php
@@ -89,10 +89,10 @@ class HomeResource extends JsonResource
             $data['company']['subtitle']['ru'] = 'Нам доверяют';
             $data['company']['subtitle']['uz'] = 'Bizga ishonishadi';
             $data['company']['subtitle']['en'] = 'They trust us';
-            $data['company']['company_content'][$i]['name'][$lang] = $settings->{'name1'. "_$lang"} ?? '';
-            $data['company']['company_content'][$i]['text'][$lang] = $settings->{'text5'. "_$lang"} ?? '';
-            $data['company']['company_content'][$i]['name'][$lang] = $settings->{'name2'. "_$lang"} ?? '';
-            $data['company']['company_content'][$i]['text'][$lang] = $settings->{'text6'. "_$lang"} ?? '';
+            $data['company']['company_content'][0]['name'][$lang] = $settings->{'name1'. "_$lang"} ?? '';
+            $data['company']['company_content'][0]['text'][$lang] = $settings->{'text5'. "_$lang"} ?? '';
+            $data['company']['company_content'][1]['name'][$lang] = $settings->{'name2'. "_$lang"} ?? '';
+            $data['company']['company_content'][1]['text'][$lang] = $settings->{'text6'. "_$lang"} ?? '';
 
             $data['company']['images'] = $companyImages;
             $data['company']['buttons']['left']['text'][$lang] = $buttons->{'company_link_text_' . $lang} ?? '';
