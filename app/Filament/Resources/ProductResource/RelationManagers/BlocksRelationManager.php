9fdebfa0ec84b1c457c9f8470a0747816dd6de85
Bexruz999 <bexruzfatullayev999@gmail.com>
2025-09-03 12:50:37 +0500

    fix: mark 'name' field as required in BlocksRelationManager

--- app/Filament/Resources/ProductResource/RelationManagers/BlocksRelationManager.php
+++ app/Filament/Resources/ProductResource/RelationManagers/BlocksRelationManager.php
@@ -33,7 +33,7 @@ class BlocksRelationManager extends RelationManager
                     ->localeTabSchema(fn(TranslatableTab $tab) => [
                         TextInput::make($tab->makeName('name'))
                             ->required($tab->makeName('name') === 'uz.name')
-                            ->label(__('form.name')),
+                            ->label(__('form.name'))->required(),
                         Repeater::make($tab->makeName('options'))->schema([
                             TextInput::make('text')
                                 ->label(__('form.item'))
