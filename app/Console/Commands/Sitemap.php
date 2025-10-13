62ff2a6813708c5ea916a2feb7a76034b7f0b8dc
Bexruz999 <bexruzfatullayev999@gmail.com>
2025-10-13 12:02:14 +0500

    fix: update sitemap URLs to include the base domain for all languages

--- app/Console/Commands/Sitemap.php
+++ app/Console/Commands/Sitemap.php
@@ -32,21 +32,22 @@ class Sitemap extends Command
     {
 
         $pages = \Spatie\Sitemap\Sitemap::create();
+        $d = 'https://ais.uz';
 
         foreach (['ru', 'en', 'uz'] as $lang) {
-            $pages->add(Url::create("/$lang/")->setLastModificationDate(Carbon::yesterday()))
-                ->add(Url::create("/$lang/about")->setLastModificationDate(Carbon::yesterday()))
-                ->add(Url::create("/$lang/catalog")->setLastModificationDate(Carbon::yesterday()))
-                ->add(Url::create("/$lang/contact")->setLastModificationDate(Carbon::yesterday()))
-                ->add(Url::create("/$lang/guarantee")->setLastModificationDate(Carbon::yesterday()))
-                ->add(Url::create("/$lang/news")->setLastModificationDate(Carbon::yesterday()))
-                ->add(Url::create("/$lang/rental")->setLastModificationDate(Carbon::yesterday()))
-                ->add(Url::create("/$lang/catalog")->setLastModificationDate(Carbon::yesterday()));
+            $pages->add(Url::create("$d/$lang/")->setLastModificationDate(Carbon::yesterday()))
+                ->add(Url::create("$d/$lang/about")->setLastModificationDate(Carbon::yesterday()))
+                ->add(Url::create("$d/$lang/catalog")->setLastModificationDate(Carbon::yesterday()))
+                ->add(Url::create("$d/$lang/contact")->setLastModificationDate(Carbon::yesterday()))
+                ->add(Url::create("$d/$lang/guarantee")->setLastModificationDate(Carbon::yesterday()))
+                ->add(Url::create("$d/$lang/news")->setLastModificationDate(Carbon::yesterday()))
+                ->add(Url::create("$d/$lang/rental")->setLastModificationDate(Carbon::yesterday()))
+                ->add(Url::create("$d/$lang/catalog")->setLastModificationDate(Carbon::yesterday()));
         }
 
-        Event::all()->each(function (Event $event) use ($pages) {
+        Event::all()->each(function (Event $event) use ($pages, $d) {
             foreach (['uz', 'en'] as $lang) {
-                $pages->add(Url::create("/$lang/news/$event->id")->setLastModificationDate(Carbon::yesterday())->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)->setPriority(0.8));
+                $pages->add(Url::create("$d/$lang/news/$event->id")->setLastModificationDate(Carbon::yesterday())->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)->setPriority(0.8));
             }
         });
 
@@ -54,24 +55,24 @@ class Sitemap extends Command
 
         $sitemap = \Spatie\Sitemap\Sitemap::create();
 
-        Category::all()->each(function (Category $category) use ($sitemap) {
+        Category::all()->each(function (Category $category) use ($sitemap, $d) {
             foreach (['ru', 'uz', 'en'] as $lang) {
                 $sitemap->add(
-                    Url::create("/$lang/catalog/$category->slug")
+                    Url::create("$d/$lang/catalog/$category->slug")
                         ->setLastModificationDate(Carbon::yesterday())
                         ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
                         ->setPriority(0.8)
                 );
                 foreach ($category->products as $product) {
                     $sitemap->add(
-                        Url::create("/$lang/catalog/$category->slug/$product->slug")
+                        Url::create("$d/$lang/catalog/$category->slug/$product->slug")
                             ->setLastModificationDate(Carbon::yesterday())
                             ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
                             ->setPriority(0.8)
                     );
                     foreach ($product->variants as $variant) {
                         $sitemap->add(
-                            Url::create("/$lang/catalog/$category->slug/$product->slug/$variant->id")
+                            Url::create("$d/$lang/catalog/$category->slug/$product->slug/$variant->id")
                                 ->setLastModificationDate(Carbon::yesterday())
                                 ->setChangeFrequency(Url::CHANGE_FREQUENCY_DAILY)
                                 ->setPriority(0.8)
